68acc976ee4bce0d505b7689eabe00808323aee2
Jeremy <jembyfowler@gmail.com>
2023-08-16 09:34:31 -0600

    init

---
+++ postit_csharp/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace postit_csharp.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
